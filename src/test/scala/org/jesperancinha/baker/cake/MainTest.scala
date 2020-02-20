3f23075a49d5415da06e2371cd841486e99d3ddf
Joao Esperancinha <jofisaes@gmail.com>
2020-02-21 00:16:46 +0100

    Fires Events

--- src/test/scala/org/jesperancinha/baker/cake/MainTest.scala
+++ src/test/scala/org/jesperancinha/baker/cake/MainTest.scala
@@ -3,15 +3,15 @@ package org.jesperancinha.baker.cake
 import akka.actor.ActorSystem
 import com.ing.baker.compiler.RecipeCompiler
 import com.ing.baker.runtime.scaladsl.{Baker, EventInstance}
-import org.jesperancinha.baker.cake.Recipes.{addColdWaterInstance, cookBeansInstance, cutPodsInHalfInstance, drainBeansInstance, fryPodsInstance, makeBatterInstance, passPodsThroughBatterInstance, peixinhosDaHortaRecipe, removeBeanThreadInstance, seasonBatterInstance, setupCookingTableInstance, washBeansInstance}
+import org.jesperancinha.baker.cake.Recipes._
 import org.scalatest.Matchers
 import org.scalatest.flatspec.AnyFlatSpec
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 import scala.language.postfixOps
-import scala.concurrent._
-import ExecutionContext.Implicits.global
+
 class MainTest extends AnyFlatSpec with Matchers {
 
   implicit val actorSystem = ActorSystem("peixinhosDaHorta")
@@ -44,13 +44,13 @@ class MainTest extends AnyFlatSpec with Matchers {
       recipeInstanceId = "my-id"
       _ <- baker.bake(recipeId, recipeInstanceId)
     } yield ()
-
+    val unit: Unit = Await.result(program, 5 seconds)
     baker.fireEventAndResolveWhenCompleted("my-id", EventInstance.unsafeFrom(Recipes.familyIsHungry))
     baker.fireEventAndResolveWhenCompleted("my-id", EventInstance.unsafeFrom(Recipes.dinnerTime))
-    val unit: Unit = Await.result(program, 5 seconds)
+    Thread.sleep(5000)
     val completeGraph = Await.result(baker.getVisualState("my-id"), 5 seconds)
+
     println(unit)
     println(completeGraph)
-
   }
 }
